08458567087dbd5cd7c5759f4152cf9bc31620a1
radulski <rafael.radulski@gmail.com>
2020-10-20 11:52:16 -0300

    commit

---
+++ src/app/layouts/default/default.module.ts
@@ -0,0 +1,31 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { SharedModule } from './../../shared/shared.module';
+import { PostsComponent } from './../../modules/posts/posts.component';
+import { DashboardComponent } from './../../modules/dashboard/dashboard.component';
+import { DefaultComponent } from './default.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+
+@NgModule({
+  declarations: [
+    DefaultComponent,
+    DashboardComponent,
+    PostsComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatButtonModule
+  ]
+})
+export class DefaultModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,38 @@
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatMenuModule,
+    MatListModule,
+    RouterModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ]
+})
+export class SharedModule { }
